d92e09afeca3157189975aa520c5fc285e66bb92
JhossyB <jhossymarbalderrama@gmail.com>
2020-12-01 10:48:34 -0300

    Recuperatorios Tps

---
+++ TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            this.cmbOperador.SelectedItem = "+";
+        }
+
+
+        /// <summary>
+        /// Boton Operar, llama a Operar para hacer dicha operacion, luego lo muestra en resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double valor;
+
+            if(this.cmbOperador.Text == "")
+            {
+                this.cmbOperador.Text = "+";
+            }
+
+            valor = Operar(this.txtNumero1.Text, this.txtNumero2.Text, this.cmbOperador.Text[0]);
+            
+
+            if (valor == double.MinValue)
+            {                
+                this.lblResultado.Text = "Error.";
+            }
+            else
+            {
+                this.lblResultado.Text = Convert.ToString(valor);
+            }         
+        }
+         
+
+        /// <summary>
+        /// Boton Limpiar pantalla, limpia todo lo q tengo en los txtbox , operador y resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarPantalla();
+        }
+
+        /// <summary>
+        /// Boton Cerrar, llama al evento Close()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        /// <summary>
+        /// Boton, Convertir de decimal a Binario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.DecimalBinario(this.lblResultado.Text);
+        }
+
+
+        /// <summary>
+        /// Boton, Comverti de binario a Decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.BinarioDecimal(this.lblResultado.Text);          
+        }
+
+
+        /// <summary>
+        ///  Obtiene los Valores 1 y 2 y el Operador
+        /// </summary>
+        /// <param name="numero1">valor 1</param>
+        /// <param name="numero2">valor 2</param>
+        /// <param name="operador">Operador(-,+,/,*)</param>
+        /// <returns></returns>
+        private static double Operar(string numero1, string numero2, char operador)
+        {
+            
+            Numero numeroUno = new Numero(numero1);
+            Numero numeroDos = new Numero(numero2);
+            
+            return Calculadora.Operar(numeroUno, numeroDos, operador);
+        }
+
+        /// <summary>
+        /// Limpia la Pantalla, setea todos los campos en vacio
+        /// </summary>
+        private void LimpiarPantalla()
+        {
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.cmbOperador.Text = "";
+            this.lblResultado.Text = "";
+        }
+
+
+        /// <summary>
+        /// Evento Form Closing, Una mensaje si esta seguro salir de la aplicacion
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult res = MessageBox.Show("¿Seguro de querer salir?","Salir",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if(res == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Evento Load, Limpia la pantalla al cargar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            LimpiarPantalla();
+        }
+    }
+}
---
+++ TP3/TestUnitario/UnitTest1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using Clases_Instanciables;
+using Excepciones;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        /// <summary>
+        /// Verificacion de DNI Incorrectos
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void VerificaNacionalidadesInvalida()
+        {
+            string dniIncorrectoExt = "10000000";
+            string dniIncorrectoArg = "99999999";
+
+
+            Alumno a1 = new Alumno(1, "Ana", "Lopez", dniIncorrectoExt, Persona.ENacionalidad.Extranjero, Universidad.EClases.Laboratorio);
+            Assert.AreEqual(dniIncorrectoExt, a1.DNI);
+
+            Alumno a2 = new Alumno(2, "Jose", "Maria", dniIncorrectoArg, Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+            Assert.AreEqual(dniIncorrectoArg, a2.DNI);
+
+        }
+
+        /// <summary>
+        /// Verificacion de Alumno Repetidos
+        /// </summary>
+        [TestMethod]
+        public void VerificaAlumnoRepetido()
+        {
+            try
+            {
+                Universidad u = new Universidad();
+
+                Alumno a1 = new Alumno(1, "Pepe", "Argento", "22222222", Persona.ENacionalidad.Argentino, Universidad.EClases.SPD);
+                Alumno a2 = new Alumno(1, "Pepe", "Argento", "22222222", Persona.ENacionalidad.Argentino, Universidad.EClases.SPD);
+
+                u += a1;
+                u += a2;
+            }
+            catch(Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(AlumnoRepetidoException));
+            }
+
+        }
+
+        /// <summary>
+        /// Validacion de DNI con formato incorrecto
+        /// Coma
+        /// Letras
+        /// Arroja DniInvalidoException
+        /// </summary>
+        [TestMethod]
+        public void DniInvalido()
+        {
+            string dniComa = "89.999,999";
+            string dniTexto = "45a54545";
+
+            try
+            {
+                Alumno personaPrimero = new Alumno(1, "juan", "perez", dniComa, Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+
+                Assert.Fail("Dni Invalido: {0}.", dniComa);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+
+            try
+            {
+                Alumno personaPrimero = new Alumno(1, "maria", "gomez", dniComa, Persona.ENacionalidad.Extranjero, Universidad.EClases.Laboratorio);
+
+
+                Assert.Fail("Dni invalido: {0}.", dniTexto);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+        }
+
+
+
+        /// <summary>
+        /// Lista Jornadas Correcta
+        /// </summary>
+        [TestMethod]
+        public void ListaJornadasCorrecta()
+        {
+            Universidad u = new Universidad();
+            Assert.IsNotNull(u.Jornadas);
+        }
+    }
+}
